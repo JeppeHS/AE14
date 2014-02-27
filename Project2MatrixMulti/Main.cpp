f6e0877f61589954d324fdc79a1d00f0ad873c3e
Laurids <laurids.mj@gmail.com>
2014-02-27 12:45:13 +0100

    Added a class for perfstat and timing stuff

--- Project2MatrixMulti/Main.cpp
+++ Project2MatrixMulti/Main.cpp
@@ -1,11 +1,31 @@
 #include "PerfStatClass.h"
 
+using std::cout;
+using std::endl;
 
-class Main : public PerfStatClass
+int main(int argc, char **argv)
 {
-  int main(int argc, char **argv)
-  {
+  PerfStatClass perf = PerfStatClass();
 
-    return 0;
+  perf.perf_event_reset();
+  perf.perf_event_enable();
+
+  perf.startTimeNow();
+
+  perf.endTimeNow();
+  
+  perf.perf_event_disable();
+  
+  const int nStats = perf.getNumberOfStats();
+  long long stats[nStats];
+  perf.read_all(stats);
+  for (int i=0; i<nStats; i++){
+    cout<<stats[i]<<endl;
   }
-};
+
+  long long time = perf.getTimeDiff();
+
+  cout << time << endl;
+  
+  return 0;
+}
