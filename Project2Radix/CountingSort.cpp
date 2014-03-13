fa4ed79d741b19858fa4e41bc52e92ee1a0eeb1f
Morten Sørensen <glantie@hotmail.com>
2014-03-13 15:57:55 +0100

    CountingSort Delete array

--- Project2Radix/CountingSort.cpp
+++ Project2Radix/CountingSort.cpp
@@ -4,16 +4,20 @@
 
 #include "CountingSort.h"
 
+int* result;
 CountingSort::CountingSort(){}
 
-CountingSort::~CountingSort(){}
+CountingSort::~CountingSort(){
+}
 
 void CountingSort::setup(int arrSize){}
 int* CountingSort::sort(int* array, int arrSize)
 {
+	delete [] result;
+	result = nullptr;
 	int gKey = 0;
     int i, j;
-	int* result = new int[arrSize];
+	result = new int[arrSize];
 	for ( int i = 0; i < arrSize; i++)
     {
 		 if(array[i] > gKey)
@@ -21,22 +25,23 @@ int* CountingSort::sort(int* array, int arrSize)
             gKey = array[i];
          }
     }
+	gKey++;
 	int* C = new int[gKey];
-	for(i = 0; i <= gKey; i++)
+	for(i = 0; i < gKey; i++)
             C[i] = 0;
-	for(j =0; j <= arrSize-1; j++)
+	for(j =0; j < arrSize; j++)
             C[array[j]] = C[array[j]] + 1;
-	for(i = 1; i <= gKey; i++)
+	for(i = 1; i < gKey; i++)
             C[i] = C[i] + C[i-1];
     for(j = arrSize-1; j >= 0; j--)
     {
-            result[C[array[j]]] = array[j];
-            C[array[j]] = C[array[j]] - 1;
+		result[C[array[j]]-1] = array[j];
+		C[array[j]] = C[array[j]] - 1;
     }
-	/* Print
     std::cout << "\nThe Sorted array is : ";
-	for(i = 1; i <= arrSize; i++)
+	for(i = 0; i < arrSize; i++)
             std::cout << result[i] << "  " ;
-			*/
+	delete [] C;
+	C = nullptr;
 	return result;
 }
